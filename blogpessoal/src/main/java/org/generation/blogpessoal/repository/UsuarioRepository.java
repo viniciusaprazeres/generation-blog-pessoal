285cb96358c87ec5522167d1435e86da6660fdec
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-29 14:57:37 -0300

    Alteração nas classes Usuario e UsuarioRepository
    Criação dos métodos Construtores na classe Usuario (Camada Model) e atualização da interface UsuarioRepository.

--- blogpessoal/src/main/java/org/generation/blogpessoal/repository/UsuarioRepository.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/repository/UsuarioRepository.java
@@ -2,8 +2,10 @@ package org.generation.blogpessoal.repository;
 
 import org.generation.blogpessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     public Optional<Usuario> findByUsuario(String usuario);
 
+    public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
